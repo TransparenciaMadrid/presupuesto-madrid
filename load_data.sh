f1913d5f268a3154ff28d002bbfce8cea332fe1f
Carlos Rodríguez <diablocharly@hotmail.com>
2026-04-28 14:20:01 +0200

    add script load data

---
+++ load_data.sh
@@ -0,0 +1,172 @@
+source bin/activate
+
+echo "load_budget-es-2011-2025"
+python manage.py load_budget 2011 --language=es
+python manage.py load_budget 2012 --language=es
+python manage.py load_budget 2013 --language=es
+python manage.py load_budget 2014 --language=es
+python manage.py load_budget 2015 --language=es
+python manage.py load_budget 2016 --language=es
+python manage.py load_budget 2017 --language=es
+python manage.py load_budget 2018 --language=es
+python manage.py load_budget 2019 --language=es
+python manage.py load_budget 2020 --language=es
+python manage.py load_budget 2021 --language=es
+python manage.py load_budget 2022 --language=es
+python manage.py load_budget 2023 --language=es
+python manage.py load_budget 2024 --language=es
+python manage.py load_budget 2025 --language=es
+
+echo "load_budget-en-2011-2025"
+python manage.py load_budget 2011 --language=en
+python manage.py load_budget 2012 --language=en
+python manage.py load_budget 2013 --language=en
+python manage.py load_budget 2014 --language=en
+python manage.py load_budget 2015 --language=en
+python manage.py load_budget 2016 --language=en
+python manage.py load_budget 2017 --language=en
+python manage.py load_budget 2018 --language=en
+python manage.py load_budget 2019 --language=en
+python manage.py load_budget 2020 --language=en
+python manage.py load_budget 2021 --language=en
+python manage.py load_budget 2022 --language=en
+python manage.py load_budget 2023 --language=en
+python manage.py load_budget 2024 --language=en
+python manage.py load_budget 2025 --language=en
+
+echo "load_payments-es-2011-2025"
+python manage.py load_payments 2011 --language=es
+python manage.py load_payments 2012 --language=es
+python manage.py load_payments 2013 --language=es
+python manage.py load_payments 2014 --language=es
+python manage.py load_payments 2015 --language=es
+python manage.py load_payments 2016 --language=es
+python manage.py load_payments 2017 --language=es
+python manage.py load_payments 2018 --language=es
+python manage.py load_payments 2019 --language=es
+python manage.py load_payments 2020 --language=es
+python manage.py load_payments 2021 --language=es
+python manage.py load_payments 2022 --language=es
+python manage.py load_payments 2023 --language=es
+python manage.py load_payments 2024 --language=es
+python manage.py load_payments 2025 --language=es
+
+echo "load_payments-en-2011-2025"
+python manage.py load_payments 2011 --language=en
+python manage.py load_payments 2012 --language=en
+python manage.py load_payments 2013 --language=en
+python manage.py load_payments 2014 --language=en
+python manage.py load_payments 2015 --language=en
+python manage.py load_payments 2016 --language=en
+python manage.py load_payments 2017 --language=en
+python manage.py load_payments 2018 --language=en
+python manage.py load_payments 2019 --language=en
+python manage.py load_payments 2020 --language=en
+python manage.py load_payments 2021 --language=en
+python manage.py load_payments 2022 --language=en
+python manage.py load_payments 2023 --language=en
+python manage.py load_payments 2024 --language=en
+python manage.py load_payments 2025 --language=en
+
+echo "load_investments-es-2011-2025"
+python manage.py load_investments 2011 --language=es
+python manage.py load_investments 2012 --language=es
+python manage.py load_investments 2013 --language=es
+python manage.py load_investments 2014 --language=es
+python manage.py load_investments 2015 --language=es
+python manage.py load_investments 2016 --language=es
+python manage.py load_investments 2017 --language=es
+python manage.py load_investments 2018 --language=es
+python manage.py load_investments 2019 --language=es
+python manage.py load_investments 2020 --language=es
+python manage.py load_investments 2021 --language=es
+python manage.py load_investments 2022 --language=es
+python manage.py load_investments 2023 --language=es
+python manage.py load_investments 2024 --language=es
+python manage.py load_investments 2025 --language=es
+
+echo "load_investments-en-2011-2025"
+python manage.py load_investments 2011 --language=en
+python manage.py load_investments 2012 --language=en
+python manage.py load_investments 2013 --language=en
+python manage.py load_investments 2014 --language=en
+python manage.py load_investments 2015 --language=en
+python manage.py load_investments 2016 --language=en
+python manage.py load_investments 2017 --language=en
+python manage.py load_investments 2018 --language=en
+python manage.py load_investments 2019 --language=en
+python manage.py load_investments 2020 --language=en
+python manage.py load_investments 2021 --language=en
+python manage.py load_investments 2022 --language=en
+python manage.py load_investments 2023 --language=en
+python manage.py load_investments 2024 --language=en
+python manage.py load_investments 2025 --language=en
+
+echo "load_main_investments-es-2011-2025"
+python manage.py load_main_investments 2011 --language=es
+python manage.py load_main_investments 2012 --language=es
+python manage.py load_main_investments 2013 --language=es
+python manage.py load_main_investments 2014 --language=es
+python manage.py load_main_investments 2015 --language=es
+python manage.py load_main_investments 2016 --language=es
+python manage.py load_main_investments 2017 --language=es
+python manage.py load_main_investments 2018 --language=es
+python manage.py load_main_investments 2019 --language=es
+python manage.py load_main_investments 2020 --language=es
+python manage.py load_main_investments 2021 --language=es
+python manage.py load_main_investments 2022 --language=es
+python manage.py load_main_investments 2023 --language=es
+python manage.py load_main_investments 2024 --language=es
+python manage.py load_main_investments 2025 --language=es
+
+echo "load_main_investments-en-2011-2025"
+python manage.py load_main_investments 2011 --language=en
+python manage.py load_main_investments 2012 --language=en
+python manage.py load_main_investments 2013 --language=en
+python manage.py load_main_investments 2014 --language=en
+python manage.py load_main_investments 2015 --language=en
+python manage.py load_main_investments 2016 --language=en
+python manage.py load_main_investments 2017 --language=en
+python manage.py load_main_investments 2018 --language=en
+python manage.py load_main_investments 2019 --language=en
+python manage.py load_main_investments 2020 --language=en
+python manage.py load_main_investments 2021 --language=en
+python manage.py load_main_investments 2022 --language=en
+python manage.py load_main_investments 2023 --language=en
+python manage.py load_main_investments 2024 --language=en
+python manage.py load_main_investments 2025 --language=en
+
+echo "load_monitoring-es-2011-2025"
+python manage.py load_monitoring 2011 --language=es
+python manage.py load_monitoring 2012 --language=es
+python manage.py load_monitoring 2013 --language=es
+python manage.py load_monitoring 2014 --language=es
+python manage.py load_monitoring 2015 --language=es
+python manage.py load_monitoring 2016 --language=es
+python manage.py load_monitoring 2017 --language=es
+python manage.py load_monitoring 2018 --language=es
+python manage.py load_monitoring 2019 --language=es
+python manage.py load_monitoring 2020 --language=es
+python manage.py load_monitoring 2021 --language=es
+python manage.py load_monitoring 2022 --language=es
+python manage.py load_monitoring 2023 --language=es
+python manage.py load_monitoring 2024 --language=es
+python manage.py load_monitoring 2025 --language=es
+
+echo "load_monitoring-en-2011-2025"
+python manage.py load_monitoring 2011 --language=en
+python manage.py load_monitoring 2012 --language=en
+python manage.py load_monitoring 2013 --language=en
+python manage.py load_monitoring 2014 --language=en
+python manage.py load_monitoring 2015 --language=en
+python manage.py load_monitoring 2016 --language=en
+python manage.py load_monitoring 2017 --language=en
+python manage.py load_monitoring 2018 --language=en
+python manage.py load_monitoring 2019 --language=en
+python manage.py load_monitoring 2020 --language=en
+python manage.py load_monitoring 2021 --language=en
+python manage.py load_monitoring 2022 --language=en
+python manage.py load_monitoring 2023 --language=en
+python manage.py load_monitoring 2024 --language=en
+python manage.py load_monitoring 2025 --language=en
+
